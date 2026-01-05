58f06dcbb48c3910a7a9ea72e990b99fe27b932c
ShakhBek <shakhandijan@gmail.com>
2026-01-05 15:41:15 +0500

    05.01 15.41

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelectorAll('.story-btn').forEach((btn) => {
-  btn.addEventListener;
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('change');
+    btn.nextElementSibling.classList.toggle('change');
+  });
 });
